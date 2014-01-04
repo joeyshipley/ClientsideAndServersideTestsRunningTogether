89ab3240ab43503fda196bf60745e47020579b69
Joey Shipley <joseph.shipley@agilethought.com>
2014-01-03 20:03:23 -0500

    Cleaning up project, removing a bunch of un-needed stuff for this example.

--- Webclient/Global.asax.cs
+++ Webclient/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Webclient
@@ -13,9 +8,17 @@ namespace Webclient
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            registerRoutes(RouteTable.Routes);
+        }
+        private void registerRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
